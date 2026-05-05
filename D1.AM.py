2b0c2d865c24129fa744ae255a40e1746fda20f7
SudhanshuBiswas01 <sudhanshubiswas2003@gmail.com>
2026-05-05 12:44:29 +0530

    Update agent with executor function

--- D1.AM.py
+++ D1.AM.py
@@ -44,10 +44,41 @@ def planner(state: AgentState) -> AgentState:
         print(f"Task {i+1}: {t}")
     return {**state, "tasks": tasks}
 
+def executor(state: AgentState) -> AgentState:
+    results = []
+    critique_ctx = ""
+    if state["critique"]:
+        critique_ctx = f"\n\n Your previous attempt was rejected. Critique: {state['critique']} \n Improve your results based on this feedback."
+
+    for task in state["tasks"]:
+        system = f"""You are an execution agent. Your task is to execute the given task using web search and return a concise summary of the findings. {critique_ctx}"""
+
+        search_ctx = ""
+        try:
+            search_results = search.run(task[:100])
+            search_ctx = f"\n\n Web Search results for context : {search_results[: 800]}"
+        except:
+            pass
+
+        message = [
+            SystemMessage(content=system),
+            HumanMessage(content=f"Task: {task} {search_ctx}")
+        ]
+
+        result = llm.invoke(message).content
+        results.append(result)
+        print(f"\n[Execution] Task: {task[:60]} \nResult: {result[:120]}")
+
+    return {**state, "results": results , "iterations": state["iterations"] + 1}
+
 graph = StateGraph(AgentState)
+
 graph.add_node("planner", planner)
+graph.add_node("executor", executor)
+
 graph.add_edge(START, "planner")
-graph.add_edge("planner", END)
+graph.add_edge("planner", "executor")
+graph.add_edge("executor", END)
 app = graph.compile()
 # - run it —
 initial_state: AgentState = {
@@ -64,3 +95,5 @@ print("\n====== FINAL OUTPUT =====")
 for i, (task, result) in enumerate(zip(final_state["tasks"], final_state["results"])):
     print(f"\n[Task {i+1}] {task} \n{result}")
 print(f"\nCompleted in {final_state['iterations']} iteration(s).")
+
+
